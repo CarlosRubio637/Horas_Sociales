be364707db260a78620d0e0dc9ba2707982a7f4e
Suni <00010124@uca.edu.sv>
2025-10-31 15:12:26 -0600

    :lipstick: Se Agrego el componente body y reestructuro los componetes y paginas

---
+++ Frontend-PW/src/components/Body_desing/Body.tsx
@@ -0,0 +1,16 @@
+import NavBar from '../Navbar_desing/Navbar';
+import './Body.css';
+
+const Body = ({ children }: {children: React.ReactNode}) => {
+    return(
+        <div className="body-container">
+            <NavBar></NavBar>
+                <div className="main-content">
+                    {children}
+                </div>
+        </div>
+    )
+}
+
+export default Body;
+
