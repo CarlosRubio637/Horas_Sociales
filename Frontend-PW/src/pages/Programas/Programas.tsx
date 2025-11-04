128c5689eca8930ddeb7cb1d4306b7a534966512
CarlosRubio637 <00000822@uca.edu.sv>
2025-11-04 08:20:59 -0600

    reorganizacion y estructura

---
+++ Frontend-PW/src/pages/Programas/Programas.tsx
@@ -0,0 +1,14 @@
+const Programas = () => {
+  return (
+    <div>
+      <h1>Programas de Formación</h1>
+      <p>
+        El Centro de Servicio Social de la UCA ofrece diversos programas de
+        formación para los estudiantes, enfocados en la práctica social y la
+        solidaridad.
+      </p>
+    </div>
+  );
+};
+
+export default Programas;
