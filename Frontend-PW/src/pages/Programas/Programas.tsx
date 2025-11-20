5d80e8288f255cd33a973f8b39f2748ad9e4be0f
CarlosRubio637 <00000822@uca.edu.sv>
2025-11-20 17:37:02 -0600

    se arreglo banner de "programas de formación"

--- Frontend-PW/src/pages/Programas/Programas.tsx
+++ Frontend-PW/src/pages/Programas/Programas.tsx
@@ -26,48 +26,51 @@ const Programas = () => {
       </section>
 
       <section className="info-content">
-        <h2>El Centro de Servicio Social de la UCA ofrece diversos programas de formación para los estudiantes, enfocados en la práctica social y la solidaridad.</h2>
+        <h2>¿Qué programas de formación tenemos?</h2>
       </section>
 
-      <section className="information-option">
-        {/* Botón y expansión para Refuerzo Academico */}
-        <button
-          className="expanding-button"
-          onClick={() => toggleProgram("RefuerzoAcademico")}
-        >
-          Círculos de Estudio
-        </button>
-        {isProgramExpanded.RefuerzoAcademico && (
-          <div className="program-details">
-            <p>Objetivo general:</p>
-            <p>Brindar apoyo académico a estudiantes de secundaria y universidad que presentan dificultades en asignaturas clave de sus estudios, contribuyendo a mejorar su desempeño académico y motivación para seguir adelante con sus estudios.</p>
+<div className="information-box">
+        <section className="information-option">
+          {/* Botón y expansión para Refuerzo Academico */}
+          <button
+            className="expanding-button"
+            onClick={() => toggleProgram("RefuerzoAcademico")}
+          >
+            Círculos de Estudio
+          </button>
+          {isProgramExpanded.RefuerzoAcademico && (
+            <div className="program-details">
+              <p>Objetivo general:</p>
+              <p>Brindar apoyo académico a estudiantes de secundaria y universidad que presentan dificultades en asignaturas clave de sus estudios, contribuyendo a mejorar su desempeño académico y motivación para seguir adelante con sus estudios.</p>
 
-            <p>¿De qué se trata este programa?</p>
-            <p>Este programa busca incentivar la superación académica de jóvenes que enfrentan barreras económicas o personales para acceder a una educación de calidad. Los estudiantes, bajo la supervisión de tutores y en coordinación con los docentes, reciben apoyo personalizado en asignaturas como matemáticas, ciencias, lengua y literatura, entre otras.</p>
+              <p>¿De qué se trata este programa?</p>
+              <p>Este programa busca incentivar la superación académica de jóvenes que enfrentan barreras económicas o personales para acceder a una educación de calidad. Los estudiantes, bajo la supervisión de tutores y en coordinación con los docentes, reciben apoyo personalizado en asignaturas como matemáticas, ciencias, lengua y literatura, entre otras.</p>
 
-            <p>El programa se desarrolla durante el año escolar, ofreciendo clases de apoyo los fines de semana o en horarios disponibles, con el acompañamiento de voluntarios del servicio social que ayudarán a los estudiantes a mejorar sus habilidades y obtener los conocimientos necesarios para superar sus exámenes y alcanzar sus objetivos académicos.</p>
-          </div>
-        )}
+              <p>El programa se desarrolla durante el año escolar, ofreciendo clases de apoyo los fines de semana o en horarios disponibles, con el acompañamiento de voluntarios del servicio social que ayudarán a los estudiantes a mejorar sus habilidades y obtener los conocimientos necesarios para superar sus exámenes y alcanzar sus objetivos académicos.</p>
+            </div>
+          )}
 
-        {/* Botón y expansión para Programa Joaquín López y López */}
-        <button
-          className="expanding-button"
-          onClick={() => toggleProgram("Voluntariado")}
-        >
-          Programa Joaquín López y López
-        </button>
-        {isProgramExpanded.Voluntariado && (
-          <div className="program-details">
-            <p>Objetivo general:</p>
-             <p> Fomentar el compromiso social de los estudiantes universitarios a través de su participación en proyectos comunitarios que buscan atender las necesidades más urgentes de los sectores menos favorecidos de la comunidad.</p>
+          {/* Botón y expansión para Programa Joaquín López y López */}
+          <button
+            className="expanding-button"
+            onClick={() => toggleProgram("Voluntariado")}
+          >
+            Programa Joaquín López y López
+          </button>
+          {isProgramExpanded.Voluntariado && (
+            <div className="program-details">
+              <p>Objetivo general:</p>
+              <p> Fomentar el compromiso social de los estudiantes universitarios a través de su participación en proyectos comunitarios que buscan atender las necesidades más urgentes de los sectores menos favorecidos de la comunidad.</p>
 
-             <p> ¿De qué se trata este programa?</p>
-             <p>Este programa involucra a los estudiantes en labores de apoyo directo en comunidades de escasos recursos. Los voluntarios trabajarán en centros de atención comunitaria, participando en actividades como la distribución de alimentos, la organización de talleres de sensibilización, el acompañamiento en actividades recreativas para niños y la asistencia en proyectos educativos. Los estudiantes en servicio social también serán responsables de organizar y gestionar actividades en los centros de apoyo.</p>
+              <p> ¿De qué se trata este programa?</p>
+              <p>Este programa involucra a los estudiantes en labores de apoyo directo en comunidades de escasos recursos. Los voluntarios trabajarán en centros de atención comunitaria, participando en actividades como la distribución de alimentos, la organización de talleres de sensibilización, el acompañamiento en actividades recreativas para niños y la asistencia en proyectos educativos. Los estudiantes en servicio social también serán responsables de organizar y gestionar actividades en los centros de apoyo.</p>
 
-             <p>El propósito principal es ayudar a las comunidades a mejorar su bienestar social mientras se promueve la solidaridad entre los estudiantes, quienes aportarán su tiempo y energía para el beneficio de la sociedad.</p>
-          </div>
-        )}
-      </section>
+              <p>El propósito principal es ayudar a las comunidades a mejorar su bienestar social mientras se promueve la solidaridad entre los estudiantes, quienes aportarán su tiempo y energía para el beneficio de la sociedad.</p>
+            </div>
+          )}
+
+        </section>
+      </div>
     </Body>
   );
 };
